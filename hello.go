a8a09df41ff7f00c86ce2856e8308590c898415c
Francisco Rodriguez <franciscojrdzrdz@gmail.com>
2023-02-11 13:38:46 -0800

    Arrays

--- hello.go
+++ hello.go
@@ -9,17 +9,26 @@ type User struct {
 }
 
 func main() {
+	// Structs
 	var u User
 	u.address = "Tijuana, Baja california"
 	u.age = 21
 	u.name = "Francisco Rodríguez"
 	fmt.Println(u)
 
+	// Loops
 	for i := 0; i < 5; i++ {
 		fmt.Printf("Number %v\n", i+1)
 	}
 
+	// Functions
 	fmt.Print(getName("Francisco Rodríguez"))
+
+	// Arrays
+	var array []int = make([]int, 100)
+	array = append(array, 10)
+	array[1] = 10
+	fmt.Print(array)
 }
 
 func getName(n string) string {
